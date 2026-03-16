a69f2efe7fd6879ace3fd3199f4e374d0ff7df1b
aprimr <aprimregmi24@gmail.com>
2026-03-16 18:27:52 +0545

    recovered project after git object corruption

---
+++ db/connect.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"os"
+
+	"github.com/aprimr/blogs-api/utils"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var Pool *pgxpool.Pool
+
+func Connect() {
+	var err error
+
+	// Get Database URL
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	// Inti connection
+	Pool, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		utils.LogFatal("Unable to connecting to database: ", err)
+	}
+
+	// Ping Database
+	err = Pool.Ping(context.Background())
+	if err != nil {
+		utils.LogFatal("Unable to ping database: ", err)
+	}
+
+	utils.LogInfo("Connected to databse")
+}
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/aprimr/blogs-api/db"
+	"github.com/aprimr/blogs-api/utils"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load .env
+	err := godotenv.Load()
+	if err != nil {
+		utils.LogFatal("Error loading .env", err)
+		return
+	}
+
+	// Connect DB
+	db.Connect()
+
+	// Create router
+	r := chi.NewRouter()
+
+	// Start server
+	port := ":" + os.Getenv("PORT")
+	utils.LogInfo("Server running on PORT" + port)
+	err = http.ListenAndServe(port, r)
+	if err != nil {
+		utils.LogFatal("Error starting Server", err)
+	}
+}
